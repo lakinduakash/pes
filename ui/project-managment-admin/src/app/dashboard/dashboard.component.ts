759ea841c8217ca7260bc72a97aa19a1850d19be
lakinduakash <lakinduakash@gmail.com>
2018-07-02 04:55:12 +0530

    add dialog

--- ui/project-managment-admin/src/app/dashboard/dashboard.component.ts
+++ ui/project-managment-admin/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {AddProjectComponent} from "../shared/add-project/add-project.component";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,13 +11,44 @@ export class DashboardComponent implements OnInit {
 
   @ViewChild('addNew') addProject: AddProjectComponent;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {
+  }
 
   ngOnInit() {
   }
 
   addNewProject() {
-    console.log("clicked")
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: "a"}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
   }
 
+}
+
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog.html'
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+export class DialogData {
+  name: String = "a";
 }
