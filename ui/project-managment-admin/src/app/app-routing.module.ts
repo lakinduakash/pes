04be4a277fcf55c294e8c29c28f6ef7c292e84b2
lakinduakash <lakinduakash@gmail.com>
2018-07-01 19:57:22 +0530

    reformat

--- ui/project-managment-admin/src/app/app-routing.module.ts
+++ ui/project-managment-admin/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 
 
-const routes:Routes =[
+const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path:'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [
     RouterModule
   ],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- ui/project-managment-admin/src/app/app.module.ts
+++ ui/project-managment-admin/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatCardModule } from '@angular/material';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AddProjectComponent } from './shared/add-project/add-project.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ProjectCardComponent } from './shared/project-card/project-card.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SideNavComponent} from './side-nav/side-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatSidenavModule} from '@angular/material';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AddProjectComponent} from './shared/add-project/add-project.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ProjectCardComponent} from './shared/project-card/project-card.component';
 
 
 @NgModule({
@@ -25,9 +25,19 @@ import { ProjectCardComponent } from './shared/project-card/project-card.compone
     ProjectCardComponent,
   ],
   imports: [
-    BrowserModule,MatToolbarModule, BrowserAnimationsModule, LayoutModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatCardModule, AppRoutingModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
