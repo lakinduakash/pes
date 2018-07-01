12cdcb2b9eed3ad0e51a44b218c4ed9bea317020
lakinduakash <lakinduakash@gmail.com>
2018-07-01 23:14:54 +0530

    add output event

--- ui/project-managment-admin/src/app/shared/add-project/add-project.component.ts
+++ ui/project-managment-admin/src/app/shared/add-project/add-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
+  @Output() addProject = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  emitClick() {
+    this.addProject.emit();
   }
 
 }
