c443e0eed650435e05e42b91174c46b5ef062233
lakinduakash <lakinduakash@gmail.com>
2018-07-01 18:54:15 +0530

    import modules

--- ui/project-managment-admin/src/app/app.module.ts
+++ ui/project-managment-admin/src/app/app.module.ts
@@ -8,17 +8,22 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatCardModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddProjectComponent } from './shared/add-project/add-project.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SideNavComponent
+    SideNavComponent,
+    DashboardComponent,
+    AddProjectComponent,
   ],
   imports: [
-    BrowserModule,MatToolbarModule, BrowserAnimationsModule, LayoutModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule
+    BrowserModule,MatToolbarModule, BrowserAnimationsModule, LayoutModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatCardModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
