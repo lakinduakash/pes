6352b1bb6987f361e46291f959f385c29de78969
lakinduakash <lakinduakash@gmail.com>
2018-07-03 19:59:32 +0530

    import

--- ui/project-managment-admin/src/app/services/project.service.ts
+++ ui/project-managment-admin/src/app/services/project.service.ts
@@ -1,10 +1,22 @@
 import {Injectable} from '@angular/core';
+import {ProjectCard} from "../model/project-card";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
+  id = 0;
+
   constructor() {
+  }
+
+  createProject(projectName) {
+    this.id++;
+    return {id: this.id, name: projectName} as ProjectCard
+  }
+
+  getProjectList() {
+    return [{id: this.id, name: "p1"}, {id: this.id, name: "p2"}]
   }
 }
