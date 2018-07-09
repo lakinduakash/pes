c7e4c2da19bb222912da513b67299039819ddaf7
lakinduakash <lakinduakash@gmail.com>
2018-07-09 23:21:29 +0530

    update last id from observable

--- ui/project-managment-admin/src/app/services/project.service.ts
+++ ui/project-managment-admin/src/app/services/project.service.ts
@@ -1,5 +1,10 @@
 import {Injectable} from '@angular/core';
 import {ProjectCard} from "../core/model/project-card";
+import {FirebaseListObservable} from "angularfire2/database-deprecated";
+import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFirestore} from "angularfire2/firestore";
+import {from} from "rxjs/internal/observable/from";
+import {Subject} from "rxjs/internal/Subject";
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +19,17 @@ export class ProjectService {
     description: "blag blah blah"
   }];
 
-  constructor() {
+  projectList: FirebaseListObservable<ProjectCard[]>;
+
+  constructor(public fireDb: AngularFireDatabase, public fireStore: AngularFirestore) {
+
   }
 
   createProject(projectCard) {
-    this.id++;
+    this.fireDb.list('/project').push(projectCard);
+
     return {
-      id: this.id,
+      id: projectCard.id,
       owner: projectCard.owner,
       cardTitle: projectCard.cardTitle,
       description: projectCard.description
@@ -28,6 +37,34 @@ export class ProjectService {
   }
 
   getProjectList() {
-    return this.ex as ProjectCard[]
+    let ref = this.fireStore.collection('project');
+
+    this.updateLastId();
+
+    ref.ref.where("id", '==', 5).onSnapshot(next => console.log(next.docs[0].id));
+    return this.projectList = this.fireDb.list('/project').valueChanges() as FirebaseListObservable<ProjectCard[]>
   }
+
+
+  getLastId() {
+    let ass = new Subject();
+
+    let ob = from(this.fireStore.collection('lastProjectId').ref.doc('DhUGQJCUsgHpUMtCMshh').get());
+    ob.subscribe(next => {
+      ass.next(next.data().last_id)
+    });
+
+    return ass
+  }
+
+  private updateLastId() {
+    this.getLastId().subscribe(next => {
+      let last_id = next;
+      console.log('lastId ' + last_id);
+      this.fireStore.collection('lastProjectId').doc('DhUGQJCUsgHpUMtCMshh').update({last_id: last_id})
+    })
+  }
+
+
 }
+
