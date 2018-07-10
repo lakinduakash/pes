d434823e6c9efa497a09529f04d957181af44fae
lakinduakash <lakinduakash@gmail.com>
2018-07-10 14:03:03 +0530

    implement CanActivate

--- ui/project-managment-admin/src/app/services/auth-guard.service.ts
+++ ui/project-managment-admin/src/app/services/auth-guard.service.ts
@@ -1,10 +1,18 @@
 import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs/internal/Observable";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+
+export class AuthGuardService implements CanActivate {
 
   constructor() {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    return undefined;
   }
 }
