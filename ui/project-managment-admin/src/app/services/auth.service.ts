beda44e7f435934c4898408c1893a2e72bcd3eaf
lakinduakash <lakinduakash@gmail.com>
2018-07-11 00:12:23 +0530

    implement login

--- ui/project-managment-admin/src/app/services/auth.service.ts
+++ ui/project-managment-admin/src/app/services/auth.service.ts
@@ -1,4 +1,7 @@
 import {Injectable} from '@angular/core';
+import {Observable} from "rxjs/internal/Observable";
+import {of} from "rxjs/internal/observable/of";
+import {delay, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,21 @@ import {Injectable} from '@angular/core';
 export class AuthService {
 
   constructor() {
+  }
+
+  isLoggedIn = false;
+
+  // store the URL so we can redirect after logging in
+  redirectUrl: string;
+
+  login(): Observable<boolean> {
+    return of(true).pipe(
+      delay(1000),
+      tap(val => this.isLoggedIn = true)
+    );
+  }
+
+  logout(): void {
+    this.isLoggedIn = false;
   }
 }
