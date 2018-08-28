7d666fb63e760cb1b5a09f4f9a4888b8a8214644
lakinduakash <lakinduakash@gmail.com>
2018-08-29 00:13:11 +0530

    add project view component

---
+++ ui/project-managment-admin/src/app/project/project-main-view-component.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-project-main-view-component',
+  templateUrl: './project-main-view-component.component.html',
+  styleUrls: ['./project-main-view-component.component.css']
+})
+export class ProjectMainViewComponentComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- ui/project-managment-admin/src/app/project/project.module.ts
+++ ui/project-managment-admin/src/app/project/project.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ProjectMainViewComponentComponent} from './project-main-view-component.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProjectMainViewComponentComponent]
 })
 export class ProjectModule {
 }
