26c2995103515fdcb41431e4686f98feff0d6909
lakinduakash <lakinduakash@gmail.com>
2018-08-28 23:50:57 +0530

    add basic form model

---
+++ ui/project-managment-admin/src/app/core/model/form-model.ts
@@ -0,0 +1,25 @@
+export class FormModel {
+  id: number;
+  name: string;
+  description: string;
+  sections: Section[]
+
+
+}
+
+export class Section {
+  id: number;
+  name: string;
+  description: string;
+  maxMarks: number;
+  currentMark: number
+
+
+}
+
+export class SectionAttribute {
+  criteria: string;
+  maxMark: number;
+  currentMark: number
+
+}
