161e276a20a4667f3268f8bf1384752b54a964ac
lakinduakash <lakinduakash@gmail.com>
2018-09-01 22:52:13 +0530

    remove form module

--- ui/project-managment-admin/src/app/eval-form/eval-form.module.ts
+++ ui/project-managment-admin/src/app/eval-form/eval-form.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormComponentComponent} from './form-component.component';
+import {FormComponent} from './form.component';
 import {SectionComponent} from './section/section.component';
-import {SectionComponentComponent} from './section-component/section-component.component';
+import {AttributeComponent} from './attribute/attribute.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FormComponentComponent, SectionComponent, SectionComponentComponent]
+  declarations: [FormComponent, SectionComponent, AttributeComponent]
 })
 export class EvalFormModule {
 }
