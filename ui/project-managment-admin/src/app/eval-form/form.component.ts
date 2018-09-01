82fdbed1ad1f46f5ecaf79b674102b4cad4e5eaa
lakinduakash <lakinduakash@gmail.com>
2018-09-02 02:05:01 +0530

    implement add and remove sections

--- ui/project-managment-admin/src/app/eval-form/form.component.ts
+++ ui/project-managment-admin/src/app/eval-form/form.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormModel, Section} from "../core/model/form-model";
 
 @Component({
   selector: 'app-form',
@@ -7,10 +8,48 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  @Input("sections") sectionList: Section[];
+  @Input("formModel") form: FormModel;
+
+  title;
+  description;
+  id;
+
+
   constructor() {
   }
 
   ngOnInit() {
+    if (this.form != undefined) {
+      this.title = this.form.name;
+      this.description = this.form.description;
+      this.id = this.form.id;
+    }
+  }
+
+  onAddSectionClick() {
+    let mSection = new Section();
+
+    if (this.sectionList != undefined) {
+      this.sectionList.push(mSection)
+    }
+    else {
+      this.sectionList = [];
+      this.sectionList.push(mSection)
+    }
+
+
+  }
+
+  onRemoveSectionClick() {
+    if (this.sectionList != undefined && this.sectionList.length > 0) {
+      this.sectionList.splice(this.sectionList.length - 1, 1)
+    }
+
+  }
+
+  renderSection() {
+
   }
 
 }
