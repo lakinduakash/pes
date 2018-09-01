c0b7e980bef7343b9e253676294389ac10f25e6f
lakinduakash <lakinduakash@gmail.com>
2018-09-01 18:16:37 +0530

    add form component to module

--- ui/project-managment-admin/src/app/eval-form/eval-form.module.ts
+++ ui/project-managment-admin/src/app/eval-form/eval-form.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormComponentComponent} from './form-component.component';
+import {SectionComponent} from './section/section.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FormComponentComponent, SectionComponent]
 })
 export class EvalFormModule {
 }
