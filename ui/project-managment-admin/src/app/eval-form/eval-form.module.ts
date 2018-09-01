7b979c8ebf28dda48b1464696a97c0c8e35ae4d6
lakinduakash <lakinduakash@gmail.com>
2018-09-02 01:16:52 +0530

    change title strings

--- ui/project-managment-admin/src/app/eval-form/eval-form.module.ts
+++ ui/project-managment-admin/src/app/eval-form/eval-form.module.ts
@@ -3,14 +3,15 @@ import {CommonModule} from '@angular/common';
 import {FormComponent} from './form.component';
 import {SectionComponent} from './section/section.component';
 import {AttributeComponent} from './attribute/attribute.component';
-import {MatCardModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import {MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDividerModule
   ],
   declarations: [FormComponent, SectionComponent, AttributeComponent],
   exports: [FormComponent]
