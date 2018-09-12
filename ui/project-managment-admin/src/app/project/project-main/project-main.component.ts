d11811f4e0aa1bb104f084afc3ee46be6f536266
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-09-12 21:39:24 +0530

    generate project-main

---
+++ ui/project-managment-admin/src/app/project/project-main/project-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-main',
+  templateUrl: './project-main.component.html',
+  styleUrls: ['./project-main.component.css']
+})
+export class ProjectMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ui/project-managment-admin/src/app/project/project.module.ts
+++ ui/project-managment-admin/src/app/project/project.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProjectMainViewComponentComponent} from './project-main-view-component.component';
 import {FormCreatorComponent} from './form-creator/form-creator.component';
+import { ProjectMainComponent } from './project-main/project-main.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProjectMainViewComponentComponent, FormCreatorComponent]
+  declarations: [ProjectMainViewComponentComponent, FormCreatorComponent, ProjectMainComponent]
 })
 export class ProjectModule {
 }
