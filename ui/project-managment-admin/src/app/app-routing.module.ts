6ac217c099e4851aa98cf417a207440135de3469
lakinduakash <lakinduakash@gmail.com>
2018-09-14 02:48:36 +0530

    add parameter to  view-form/:id

--- ui/project-managment-admin/src/app/app-routing.module.ts
+++ ui/project-managment-admin/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {TestComponent} from "./test/test.component";
 import {SignupComponent} from "./signup/signup.component";
 import {LoginComponent} from "./login/login.component";
 import {FormViewComponent} from "./eval-form-parser/form-view.component";
+import {ViewFormListComponent} from "./project/view-form-list/view-form-list.component";
 
 
 const routes: Routes = [
@@ -15,6 +16,9 @@ const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService]},
   {path: 'project/:id', component: ProjectMainViewComponentComponent, canActivate: [AuthGuardService]},
   {path: 'test', component: TestComponent},
+  {path: 'view-form/:id', component: FormViewComponent},
+  {path: 'create-form', component: TestComponent},
+  {path: 'list-form', component: ViewFormListComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
   {path: '**', component: PageNotFoundComponent}
