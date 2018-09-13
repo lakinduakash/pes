4a0750f9f6c238ba4af50d816dc3f50967d16d34
lakinduakash <lakinduakash@gmail.com>
2018-09-14 00:06:32 +0530

    add new component to form parser

--- ui/project-managment-admin/src/app/eval-form-parser/eval-form-parser.module.ts
+++ ui/project-managment-admin/src/app/eval-form-parser/eval-form-parser.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { FormViewComponent } from './form-view/form-view.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FormViewComponent]
 })
 export class EvalFormParserModule {
 }
