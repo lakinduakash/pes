f824d14c3d76fd53cbf64dfc574a7457e72105ee
lakinduakash <lakinduakash@gmail.com>
2018-09-15 02:20:42 +0530

    fix errors

--- ui/project-managment-admin/src/app/auth/auth.guard.ts
+++ ui/project-managment-admin/src/app/auth/auth.guard.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { AuthService} from './auth.service'
+import { Observable } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
+
+@Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot): Observable<boolean> {
+
+    return this.auth.user.pipe(
+    take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          console.log('access denied');
+          this.router.navigate(['/login']);
+        }
+      })
+  )
   }
 }
