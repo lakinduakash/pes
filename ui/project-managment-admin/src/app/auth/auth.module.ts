bc4de18de45f545ee4760aabe81cb7dd85780a57
lakinduakash <lakinduakash@gmail.com>
2018-09-15 02:02:07 +0530

    add auth service to module

--- ui/project-managment-admin/src/app/auth/auth.module.ts
+++ ui/project-managment-admin/src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AuthService} from "./auth.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers:[AuthService]
 })
 export class AuthModule { }
