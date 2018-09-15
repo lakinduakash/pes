0937b7f18c12e2f6d9eddd077529141b913769d7
lakinduakash <lakinduakash@gmail.com>
2018-09-15 19:57:29 +0530

    add validation to signup

--- ui/project-managment-admin/src/app/signup/signup.component.ts
+++ ui/project-managment-admin/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +8,51 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() {
+  signupFormGroup:FormGroup;
+
+
+  constructor(private fb:FormBuilder) {
+
+    this.signupFormGroup=fb.group({
+      fName:['',Validators.compose([Validators.required,Validators.maxLength(15),Validators.minLength(1)])],
+      lName:['',[Validators.required,Validators.maxLength(19)]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(8)]],
+      passwordc:['',Validators.required]
+    },
+      {
+        validator: PasswordValidation.MatchPassword
+      });
+
   }
 
   ngOnInit() {
+
   }
 
+  signup()
+  {
+
+  }
+
+  postData(data)
+  {
+    console.log(data)
+  }
+
+}
+
+import {AbstractControl} from '@angular/forms';
+export class PasswordValidation {
+
+  static MatchPassword(AC: AbstractControl) {
+    let password = AC.get('password').value; // to get value in input tag
+    let confirmPassword = AC.get('passwordc').value; // to get value in input tag
+    if(password != confirmPassword || password.length<8) {
+
+      AC.get('passwordc').setErrors( {MatchPassword: true} )
+    } else {
+      return null
+    }
+  }
 }
