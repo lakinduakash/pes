61e4f4ea6e54d6a4125295b1f4421dc8a92166c2
lakinduakash <lakinduakash@gmail.com>
2018-09-15 16:02:08 +0530

    add MatMenuModule

--- ui/project-managment-admin/src/app/shared/shared.module.ts
+++ ui/project-managment-admin/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {LayoutModule} from '@angular/cdk/layout';
-import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatListModule, MatMenuModule, MatSidenavModule} from '@angular/material';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {SideNavComponent} from "./side-nav/side-nav.component";
 
@@ -19,6 +19,7 @@ import {SideNavComponent} from "./side-nav/side-nav.component";
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatMenuModule
   ],
   providers: [],
   exports: [
