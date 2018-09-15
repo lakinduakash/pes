9ba25a0b31873678cbbe32d5bc45e8eb3b8c08e5
lakinduakash <lakinduakash@gmail.com>
2018-09-15 16:02:48 +0530

    add authGurds

--- ui/project-managment-admin/src/app/app-routing.module.ts
+++ ui/project-managment-admin/src/app/app-routing.module.ts
@@ -2,23 +2,24 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {AuthGuardService} from "./services/auth-guard.service";
+
 import {ProjectMainViewComponentComponent} from "./project/project-main-view-component.component";
 import {TestComponent} from "./test/test.component";
 import {SignupComponent} from "./signup/signup.component";
 import {LoginComponent} from "./login/login.component";
 import {FormViewComponent} from "./eval-form-parser/form-view.component";
 import {ViewFormListComponent} from "./project/view-form-list/view-form-list.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService]},
-  {path: 'project/:id', component: ProjectMainViewComponentComponent, canActivate: [AuthGuardService]},
+  {path: 'dashboard', component: DashboardComponent,  canActivate:[AuthGuard]},
+  {path: 'project/:id', component: ProjectMainViewComponentComponent,canActivate:[AuthGuard] },
   {path: 'test', component: TestComponent},
-  {path: 'view-form/:id', component: FormViewComponent},
-  {path: 'create-form', component: TestComponent},
-  {path: 'list-form', component: ViewFormListComponent},
+  {path: 'view-form/:id', component: FormViewComponent,canActivate:[AuthGuard]},
+  {path: 'create-form', component: TestComponent,canActivate:[AuthGuard]},
+  {path: 'list-form', component: ViewFormListComponent,canActivate:[AuthGuard]},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
   {path: '**', component: PageNotFoundComponent}
