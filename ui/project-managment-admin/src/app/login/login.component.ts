b23192ca34d40cb65e7333881e5da92f4cfe0a29
lakinduakash <lakinduakash@gmail.com>
2018-09-15 22:34:57 +0530

    check email verification status

--- ui/project-managment-admin/src/app/login/login.component.ts
+++ ui/project-managment-admin/src/app/login/login.component.ts
@@ -19,11 +19,14 @@ export class LoginComponent implements OnInit {
   password: string;
   showSpinner=false;
 
+  emailVerifiedStatus="";
+
   isHandest$;
   inFogotFassword=false;
   successResetSend;
 
   emailSentMassage="";
+  errorMassage="";
 
   constructor(private authService:AuthService,private router:Router,private breakPointObserver:BreakpointObserver,private authf:AngularFireAuth) { }
 
@@ -35,10 +38,28 @@ export class LoginComponent implements OnInit {
   {
     this.showSpinner=true;
     this.authService.emailLogin(this.email,this.password).subscribe(next=>{
-      this.router.navigate(['/dashboard']);
+
+      this.authf.user.subscribe(next=>{
+        if(next!=undefined && next.emailVerified)
+          this.router.navigate(['/dashboard']);
+        else
+        {
+          this.emailVerifiedStatus="Email not verified, first verify the email";
+          this.authf.auth.signOut()
+        }
+
+      });
+
       this.showSpinner=false
-    },error1 => {console.log(error1);
+    },error1 => {
+      console.log(error1);
+      this.errorMassage="User name or email not correct";
     this.showSpinner=false})
+  }
+
+  checkVerificationStus()
+  {
+
   }
 
   forgotPasswordClick()
