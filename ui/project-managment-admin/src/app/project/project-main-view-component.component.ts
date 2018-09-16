b0f4f997568476f822d3b4a052cbf01f9a5dfda4
lakinduakash <lakinduakash@gmail.com>
2018-09-16 22:11:30 +0530

    implement add presentation

--- ui/project-managment-admin/src/app/project/project-main-view-component.component.ts
+++ ui/project-managment-admin/src/app/project/project-main-view-component.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {MatDialog} from "@angular/material";
+import {CreatePresentationDialogComponent} from "./create-presentation-dialog/create-presentation-dialog.component";
+import {PresentationService} from "../services/presentation.service";
+import {Presentation} from "../core/model/presentation";
 
 @Component({
   selector: 'app-project-main-view-component',
@@ -10,12 +14,36 @@ export class ProjectMainViewComponentComponent implements OnInit {
 
   id: string;
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              public dialog: MatDialog,
+              private presentationService:PresentationService) {
   }
 
   ngOnInit() {
-    console.log("hello");
+
     this.route.paramMap.subscribe(next => this.id = next.get('id'))
+
+
+  }
+
+  addPresentation(){
+    console.log(this.id);
+
+    const dialogRef = this.dialog.open(CreatePresentationDialogComponent, {
+      width: '250px',
+      panelClass:'custom-modalbox',
+      data: {name: "", description: ""}
+    });
+
+    dialogRef.componentInstance.createClick.subscribe(
+      next=>
+      {
+          this.presentationService.addPresentation(Number(this.id),{name:dialogRef.componentInstance.data.name,description:dialogRef.componentInstance.data.description} as Presentation).
+          subscribe(next=>console.log(next))
+
+      }
+    )
 
 
   }
