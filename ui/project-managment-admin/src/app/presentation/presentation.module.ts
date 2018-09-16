f1504d02c257159c797c196059018eb179af0772
lakinduakash <lakinduakash@gmail.com>
2018-09-17 02:37:51 +0530

    add dependencies

--- ui/project-managment-admin/src/app/presentation/presentation.module.ts
+++ ui/project-managment-admin/src/app/presentation/presentation.module.ts
@@ -1,12 +1,46 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PresentationComponent} from './presentation/presentation.component';
+import {SharedModule} from "../shared/shared.module";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatOptionModule,
+  MatRippleModule,
+  MatSelectModule
+} from "@angular/material";
+import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    MatListModule,
+    RouterModule,
+    SharedModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatRippleModule,
+    MatExpansionModule,
+    MatDialogModule
   ],
-  declarations: [PresentationComponent]
+  declarations: [PresentationComponent],
+  exports: [PresentationComponent]
 })
 export class PresentationModule {
 }
