e685e08cc246bb90927660c2f16aee83dca82013
lakinduakash <lakinduakash@gmail.com>
2018-09-17 04:38:17 +0530

    customise form saving path in firebase

--- ui/project-managment-admin/src/app/services/form.service.ts
+++ ui/project-managment-admin/src/app/services/form.service.ts
@@ -1,19 +1,28 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from "angularfire2/firestore";
-import {from, Observable} from "rxjs";
 import {fromPromise} from "rxjs/internal-compatibility";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  constructor(private fireStore: AngularFirestore) {
+  constructor(private fireStore: AngularFirestore, private authService: AuthService) {
+
   }
 
-  saveForm(form) {
+  saveForm(form, projectId?: string, presentId?: string) {
+    if (projectId == undefined)
+      return fromPromise(this.fireStore.collection('form').add(JSON.parse(JSON.stringify(form))))
+    else {
+      this.authService.user.subscribe(
+        next => {
+          fromPromise(this.fireStore.collection(`usersC/${next.uid}/project/${projectId}/presentation/${presentId}/form`).add(JSON.parse(JSON.stringify(form))))
+        }
+      )
 
-    return fromPromise(this.fireStore.collection('form').add(JSON.parse(JSON.stringify(form))))
+    }
   }
 
   getLastId() {
