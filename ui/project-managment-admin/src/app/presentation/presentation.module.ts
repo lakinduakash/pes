9d7b352c39fed542b08e8de47b7f20eea891d4ad
lakinduakash <lakinduakash@gmail.com>
2018-09-17 02:00:40 +0530

    add presentation component

--- ui/project-managment-admin/src/app/presentation/presentation.module.ts
+++ ui/project-managment-admin/src/app/presentation/presentation.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {PresentationComponent} from './presentation/presentation.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PresentationComponent]
 })
 export class PresentationModule {
 }
