a047f0ff6bab3e0174c6088342d8cd54e870c64f
lakinduakash <lakinduakash@gmail.com>
2018-09-17 04:38:38 +0530

    export components

--- ui/project-managment-admin/src/app/project/project.module.ts
+++ ui/project-managment-admin/src/app/project/project.module.ts
@@ -2,19 +2,25 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProjectMainViewComponentComponent} from './project-main-view-component.component';
 import {FormCreatorComponent} from './form-creator/form-creator.component';
-import { ViewFormListComponent } from './view-form-list/view-form-list.component';
+import {ViewFormListComponent} from './view-form-list/view-form-list.component';
 import {
   MatButtonModule,
-  MatCardModule, MatDialogModule, MatDividerModule, MatExpansionModule,
-  MatFormFieldModule, MatIconModule,
+  MatCardModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
   MatInputModule,
-  MatListModule, MatOptionModule,
-  MatRippleModule, MatSelectModule
+  MatListModule,
+  MatOptionModule,
+  MatRippleModule,
+  MatSelectModule
 } from "@angular/material";
 import {RouterModule} from "@angular/router";
 import {SharedModule} from "../shared/shared.module";
 import {FormsModule} from "@angular/forms";
-import { CreatePresentationDialogComponent } from './create-presentation-dialog/create-presentation-dialog.component';
+import {CreatePresentationDialogComponent} from './create-presentation-dialog/create-presentation-dialog.component';
 
 
 @NgModule({
@@ -36,7 +42,8 @@ import { CreatePresentationDialogComponent } from './create-presentation-dialog/
     MatExpansionModule,
     MatDialogModule
   ],
-  declarations: [ProjectMainViewComponentComponent, FormCreatorComponent, ViewFormListComponent, CreatePresentationDialogComponent]
+  declarations: [ProjectMainViewComponentComponent, FormCreatorComponent, ViewFormListComponent, CreatePresentationDialogComponent],
+  exports: [ViewFormListComponent]
   ,
   entryComponents:[CreatePresentationDialogComponent]
 })
