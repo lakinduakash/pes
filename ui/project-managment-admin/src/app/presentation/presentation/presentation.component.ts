63e1dc968701c0326b00392952a651ab75fb559d
lakinduakash <lakinduakash@gmail.com>
2018-09-17 02:37:10 +0530

    add presentation component

---
+++ ui/project-managment-admin/src/app/presentation/presentation/presentation.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  templateUrl: './presentation.component.html',
+  styleUrls: ['./presentation.component.css']
+})
+export class PresentationComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
