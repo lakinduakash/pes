bcc2d4c2023c80e42bcb4dc63324feebd141d743
lakinduakash <lakinduakash@gmail.com>
2018-09-16 15:59:18 +0530

    add dependencies

--- ui/project-managment-admin/src/app/project/project.module.ts
+++ ui/project-managment-admin/src/app/project/project.module.ts
@@ -3,15 +3,35 @@ import {CommonModule} from '@angular/common';
 import {ProjectMainViewComponentComponent} from './project-main-view-component.component';
 import {FormCreatorComponent} from './form-creator/form-creator.component';
 import { ViewFormListComponent } from './view-form-list/view-form-list.component';
-import {MatListModule} from "@angular/material";
+import {
+  MatButtonModule,
+  MatCardModule, MatDividerModule,
+  MatFormFieldModule, MatIconModule,
+  MatInputModule,
+  MatListModule, MatOptionModule,
+  MatRippleModule, MatSelectModule
+} from "@angular/material";
 import {RouterModule} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   imports: [
     CommonModule,
     MatListModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatRippleModule
   ],
   declarations: [ProjectMainViewComponentComponent, FormCreatorComponent, ViewFormListComponent]
 })
