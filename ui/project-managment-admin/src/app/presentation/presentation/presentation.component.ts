4a47ae8b57fb68462c4d0fd412f6a63856e743f7
lakinduakash <lakinduakash@gmail.com>
2018-09-18 05:38:47 +0530

    rename title bar and add form data service

--- ui/project-managment-admin/src/app/presentation/presentation/presentation.component.ts
+++ ui/project-managment-admin/src/app/presentation/presentation/presentation.component.ts
@@ -1,23 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {ProjectService} from "../../services/project.service";
-import {FormService} from "../../services/form.service";
+import {FormDataService} from "../../services/form-data.service";
+import {RenameTitleBarService} from "../../services/rename-title-bar.service";
 
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.css']
 })
-export class PresentationComponent implements OnInit {
+export class PresentationComponent implements OnInit, OnDestroy {
 
   projectId
   originalPId;
   presentId;
 
-  constructor(private router: Router, private route: ActivatedRoute, private projectService: ProjectService, formService: FormService) {
+  panelState: boolean
+
+  constructor(private router: Router, private route: ActivatedRoute, private projectService: ProjectService, public formDataService: FormDataService, private titleBar: RenameTitleBarService) {
   }
 
   ngOnInit() {
+    this.titleBar.setTitle("Presentations")
     this.route.parent.parent.params.subscribe(params => {
       this.projectId = Number(params.id);
       console.log(params)
@@ -26,6 +30,9 @@ export class PresentationComponent implements OnInit {
         this.projectService.getOriginalProjectId(this.projectId)
           .subscribe(next => {
             this.originalPId = next
+
+            this.formDataService.presentationId = this.presentId;
+            this.formDataService.projectId = this.originalPId;
           })
       })
 
@@ -35,6 +42,12 @@ export class PresentationComponent implements OnInit {
   }
 
   createForm() {
-    this.router.navigate(['/test'])
+
+    this.router.navigate(['/create-form'])
+  }
+
+
+  ngOnDestroy(): void {
+
   }
 }
