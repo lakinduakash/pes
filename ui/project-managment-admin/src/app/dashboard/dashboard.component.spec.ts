867469bb303d58f0dca9055008ad06b14362d016
lakinduakash <lakinduakash@gmail.com>
2018-09-28 03:11:43 +0530

    adjust some tests

--- ui/project-managment-admin/src/app/dashboard/dashboard.component.spec.ts
+++ ui/project-managment-admin/src/app/dashboard/dashboard.component.spec.ts
@@ -2,33 +2,25 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DashboardComponent} from './dashboard.component';
 import {RouterTestingModule} from "@angular/router/testing";
-import {
-  MatCardModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatInputModule,
-  MatProgressBarModule
-} from "@angular/material";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AngularFireModule} from "@angular/fire";
 import {environment} from "../../environments/environment";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AuthService} from "../auth/auth.service";
 import {AngularFireAuth} from "@angular/fire/auth";
 import {AngularFirestore} from "@angular/fire/firestore";
-import {SharedModule} from "../shared/shared.module";
 import {DashboardModule} from "./dashboard.module";
+import {DebugElement} from "@angular/core";
 
 fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [],
-      imports: [DashboardModule, RouterTestingModule, MatCardModule, MatGridListModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, SharedModule,
-        MatProgressBarModule, AngularFireModule.initializeApp(environment.firebase, 'myApp'), BrowserAnimationsModule]
-      , providers: [AuthService, AngularFireAuth, AngularFirestore]
+      imports: [DashboardModule, RouterTestingModule, AngularFireModule.initializeApp(environment.firebase, 'myApp'), BrowserAnimationsModule],
+      providers: [AuthService, AngularFireAuth, AngularFirestore]
     })
     .compileComponents();
   }));
@@ -37,6 +29,7 @@ fdescribe('DashboardComponent', () => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    de = fixture.debugElement
   });
 
   fit('should create', () => {
@@ -45,6 +38,18 @@ fdescribe('DashboardComponent', () => {
 
   fit('tool bar title is correct', () => {
     component.renameTitleBar.getTitle().subscribe(next => expect(next).toBe("Project Dashboard"))
+  })
+
+  // fit('Dialog is opened when button clicked',()=>{
+  //   //component.createDialog();
+  //   expect(fixture.nativeElement.querySelector('mat-grid-list')).toBeNull()
+  //
+  // })
+
+  fit('tool bar title is correct when add project clicked', () => {
+    component.createDialog();
+    component.renameTitleBar.getTitle().subscribe(next => expect(next).toBe("Add Project"))
+
   })
 
 });
