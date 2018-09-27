37b22560c50a8efafe9ad48af7a790483b0c457f
lakinduakash <lakinduakash@gmail.com>
2018-09-27 23:51:32 +0530

    fix tests

--- ui/project-managment-admin/src/app/dashboard/dashboard.component.spec.ts
+++ ui/project-managment-admin/src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,34 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DashboardComponent} from './dashboard.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatInputModule,
+  MatProgressBarModule
+} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../../environments/environment";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AuthService} from "../auth/auth.service";
+import {AngularFireAuth} from "@angular/fire/auth";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {SharedModule} from "../shared/shared.module";
+import {DashboardModule} from "./dashboard.module";
 
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [],
+      imports: [DashboardModule, RouterTestingModule, MatCardModule, MatGridListModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, SharedModule,
+        MatProgressBarModule, AngularFireModule.initializeApp(environment.firebase, 'myApp'), BrowserAnimationsModule]
+      , providers: [AuthService, AngularFireAuth, AngularFirestore]
     })
     .compileComponents();
   }));
@@ -19,7 +39,7 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
