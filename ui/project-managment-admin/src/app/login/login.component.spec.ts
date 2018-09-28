a0eec6ae78fbd8de8face36b39084ba9bda00613
lakinduakash <lakinduakash@gmail.com>
2018-09-28 19:10:38 +0530

    fix test

--- ui/project-managment-admin/src/app/login/login.component.spec.ts
+++ ui/project-managment-admin/src/app/login/login.component.spec.ts
@@ -1,14 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule
+} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../../environments/environment";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AuthService} from "../auth/auth.service";
+import {AngularFireAuth} from "@angular/fire/auth";
+import {AngularFirestore} from "@angular/fire/firestore";
 
-describe('LoginComponent', () => {
+fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [RouterTestingModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,
+        MatProgressBarModule, AngularFireModule.initializeApp(environment.firebase, 'myApp'), BrowserAnimationsModule,
+        MatProgressSpinnerModule]
+      , providers: [AuthService, AngularFireAuth, AngularFirestore]
     })
     .compileComponents();
   }));
@@ -19,7 +38,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
