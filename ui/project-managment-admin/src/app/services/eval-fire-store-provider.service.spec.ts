01f640290663752aa84f7788b9562e42d6c7684d
lakinduakash <lakinduakash@gmail.com>
2018-10-01 01:11:16 +0530

    add tests to second firestore

--- ui/project-managment-admin/src/app/services/eval-fire-store-provider.service.spec.ts
+++ ui/project-managment-admin/src/app/services/eval-fire-store-provider.service.spec.ts
@@ -1,12 +1,42 @@
 import {TestBed} from '@angular/core/testing';
 
-import {EvalFireStoreProviderService} from './eval-fire-store-provider.service';
+import {EvalFireStoreFactory, EvalFireStoreProviderService} from './eval-fire-store-provider.service';
+import {NgZone, PLATFORM_ID} from "@angular/core";
+import {from} from "rxjs";
 
-describe('EvalFireStoreProviderService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('EvalFireStoreProviderService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{
+        provide: EvalFireStoreProviderService,
+        deps: [PLATFORM_ID, NgZone],
+        useFactory: EvalFireStoreFactory
+      }]
+    })
+  });
 
-  it('should be created', () => {
-    const service: EvalFireStoreProviderService = TestBed.get(EvalFireStoreProviderService);
+  fit('should be created', () => {
+    let service: EvalFireStoreProviderService = TestBed.get(EvalFireStoreProviderService);
     expect(service).toBeTruthy();
   });
+
+  fit('write a value and get the value ', (done) => {
+
+    let service: EvalFireStoreProviderService = TestBed.get(EvalFireStoreProviderService);
+    from(service.collection('f').add({a: 4})).subscribe(next => {
+      from(service.collection('f').ref.where('a', '==', 4).get()).subscribe(value => {
+          let c = value.docs.length
+
+          service.collection('f').ref.doc().delete()
+
+          expect(c).toBeGreaterThanOrEqual(1)
+          done();
+
+        },
+        error1 => console.log(error1))
+
+    })
+
+
+  })
 });
