7ec687128a2e6aee01ece6eb4fb5f50d28d39d1f
lakinduakash <lakinduakash@gmail.com>
2018-10-07 03:02:01 +0530

    customize footer and side nav

--- ui/project-managment-admin/src/app/components/sidebar/sidebar.component.ts
+++ ui/project-managment-admin/src/app/components/sidebar/sidebar.component.ts
@@ -9,13 +9,10 @@ declare interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
   {path: '/dashboard', title: 'Dashboard', icon: 'design_app', class: ''},
-  {path: '/icons', title: 'Icons', icon: 'education_atom', class: ''},
-  {path: '/maps', title: 'Maps', icon: 'location_map-big', class: ''},
-  {path: '/notifications', title: 'Notifications', icon: 'ui-1_bell-53', class: ''},
+  {path: '/dashboard', title: 'Invite Evaluator', icon: 'education_atom', class: ''},
+  {path: '/dashboard', title: 'Notifications', icon: 'ui-1_bell-53', class: ''},
 
-  {path: '/user-profile', title: 'User Profile', icon: 'users_single-02', class: ''},
-  {path: '/table-list', title: 'Table List', icon: 'design_bullet-list-67', class: ''},
-  {path: '/typography', title: 'Typography', icon: 'text_caps-small', class: ''}
+  {path: '/dashboard', title: 'User Profile', icon: 'users_single-02', class: ''},
 ];
 
 @Component({
