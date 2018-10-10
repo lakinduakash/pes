2bfd8d43ef35336db2c789a79f174b1b31f8364e
lakinduakash <lakinduakash@gmail.com>
2018-10-10 21:03:41 +0530

    add nav bar rename service

---
+++ ui/project-managment-admin/src/app/components/services/nav-bar-title.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavBarTitleService {
+
+  title: BehaviorSubject<string> = new BehaviorSubject('')
+
+  constructor() {
+  }
+
+  setTitle(title: string) {
+    this.title.next(title)
+  }
+
+  getTitle(): Observable<string> {
+    return this.title as Observable
+  }
+}
