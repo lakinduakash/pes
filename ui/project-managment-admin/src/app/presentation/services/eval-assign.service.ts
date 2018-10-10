7fa1dc1663cd95cc9538f10519965bc52f79c06e
lakinduakash <lakinduakash@gmail.com>
2018-10-10 18:41:23 +0530

    add functions for EvalAssignService

--- ui/project-managment-admin/src/app/presentation/services/eval-assign.service.ts
+++ ui/project-managment-admin/src/app/presentation/services/eval-assign.service.ts
@@ -1,10 +1,34 @@
 import {Injectable} from '@angular/core';
+import {FormService} from "../../services/form.service";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {FormDataService} from "../../services/form-data.service";
+import {Observable, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EvalAssignService {
 
-  constructor() {
+  constructor(private formService: FormService, private fs: AngularFirestore, private formData: FormDataService) {
   }
+
+  assignEvaluators(data: AssigneeData) {
+    this.formService.updateForm(this.formData.projectId, this.formData.presentationId, data.formId, {assign: data.evalList})
+  }
+
+  getAssigneeList(data) {
+    let s = new Subject<any>();
+    this.formService.getForm(data.id, this.formData.projectId, this.formData.presentationId).subscribe(
+      next => s.next(next.data().assign)
+    )
+
+    return s as Observable
+  }
+
+
+}
+
+export interface AssigneeData {
+  formId: string
+  evalList: any[]
 }
