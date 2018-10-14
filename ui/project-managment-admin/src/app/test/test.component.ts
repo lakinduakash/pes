8ca57115ea9a7c30c76e5b045063468ce0a900c9
lakinduakash <lakinduakash@gmail.com>
2018-10-14 12:23:22 +0530

    test some jquery

--- ui/project-managment-admin/src/app/test/test.component.ts
+++ ui/project-managment-admin/src/app/test/test.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {CanComponentDeactivate} from "../shared/can-deactivate-guard.service";
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-test',
@@ -13,11 +14,103 @@ export class TestComponent implements OnInit, CanComponentDeactivate {
   }
 
   ngOnInit() {
+    this.jq()
   }
 
   canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
     console.log('canDeactivate has fired in the component!');
     return true;
+  }
+
+  jq() {
+
+    var getslideHeight = $('.slide.active').height();
+
+    $('.slides').css({
+      height: getslideHeight
+    });
+
+    function calcslideHeight() {
+      getslideHeight = $('.slide.active').height();
+
+      $('.slides').css({
+        height: getslideHeight
+      });
+    }
+
+    function animateContentColor() {
+      var getslideColor = $('.slide.active').attr('slide-color');
+
+      $('body').css({
+        background: getslideColor
+      });
+
+      $('.title').css({
+        color: getslideColor
+      });
+
+      $('.btn').css({
+        color: getslideColor
+      });
+    }
+
+    var slideItem = $('.slide'),
+      slideCurrentItem = slideItem.filter('.active');
+
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+
+      var nextItem = slideCurrentItem.next();
+
+      slideCurrentItem.removeClass('active');
+
+      if (nextItem.length) {
+
+        slideCurrentItem = nextItem.addClass('active');
+      } else {
+        slideCurrentItem = slideItem.first().addClass('active');
+      }
+
+      calcslideHeight();
+      animateContentColor();
+    });
+
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+
+      var prevItem = slideCurrentItem.prev();
+
+      slideCurrentItem.removeClass('active');
+
+      if (prevItem.length) {
+        slideCurrentItem = prevItem.addClass('active');
+      } else {
+        slideCurrentItem = slideItem.last().addClass('active');
+      }
+
+      calcslideHeight();
+      animateContentColor();
+    });
+
+    // Ripple
+    $('[ripple]').on('click', function (e) {
+      var rippleDiv = $('<div class="ripple" />'),
+        rippleSize = 60,
+        rippleOffset = $(this).offset(),
+        rippleY = e.pageY - rippleOffset.top,
+        rippleX = e.pageX - rippleOffset.left,
+        ripple = $('.ripple');
+
+      rippleDiv.css({
+        top: rippleY - (rippleSize / 2),
+        left: rippleX - (rippleSize / 2),
+        background: $(this).attr("ripple-color")
+      }).appendTo($(this));
+
+      window.setTimeout(function () {
+        rippleDiv.remove();
+      }, 1900);
+    });
   }
 
 }
