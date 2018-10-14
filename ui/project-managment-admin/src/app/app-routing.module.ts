b18a2b696d64f9e7a6c531ecb8b2c377c8f70dc4
lakinduakash <lakinduakash@gmail.com>
2018-10-14 20:14:16 +0530

    move routes for lazy-loading

--- ui/project-managment-admin/src/app/app-routing.module.ts
+++ ui/project-managment-admin/src/app/app-routing.module.ts
@@ -1,54 +1,25 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {DashboardComponent} from "./dashboard/dashboard.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-import {ProjectMainViewComponentComponent} from "./project/project-main-view-component.component";
 import {TestComponent} from "./test/test.component";
 import {SignupComponent} from "./signup/signup.component";
 import {LoginComponent} from "./login/login.component";
 import {FormViewComponent} from "./eval-form-parser/form-view.component";
-import {ViewFormListComponent} from "./project/view-form-list/view-form-list.component";
 import {AuthGuard} from "./auth/auth.guard";
-import {PresentationComponent} from "./presentation/presentation/presentation.component";
 import {CanDeactivateGuard} from "./shared/can-deactivate-guard.service";
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent,  canActivate:[AuthGuard]},
+  {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard]},
   {
     path: 'project',
-    children: [
-      {
-        path: ':id',
-        children:
-          [
-            {path: '', component: ProjectMainViewComponentComponent, canActivate: [AuthGuard]},
-            {
-              path: 'presentation',
-              children: [
-                {
-                  path: ':id',
-                  children: [
-                    {path: '', component: PresentationComponent, canActivate: [AuthGuard]},
-                    {path: 'form/:id', component: TestComponent, canActivate: [AuthGuard]}
-
-                  ]
-                }
-              ]
-            },
-
-          ]
-      },
-      {path: '**', component: PageNotFoundComponent}
-
-    ]
+    loadChildren: './project/project.module#ProjectModule'
   },
   {path: 'test', component: TestComponent},
   {path: 'view-form/:id', component: FormViewComponent,canActivate:[AuthGuard]},
   {path: 'create-form', component: TestComponent, canActivate: [AuthGuard], canDeactivate: [CanDeactivateGuard]},
-  {path: 'list-form', component: ViewFormListComponent,canActivate:[AuthGuard]},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
   {path: '**', component: PageNotFoundComponent}
