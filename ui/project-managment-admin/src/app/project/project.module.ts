f0407b4d30a71c029c34f6c007e21134a677b9b3
lakinduakash <lakinduakash@gmail.com>
2018-10-14 23:15:28 +0530

    fix imports

--- ui/project-managment-admin/src/app/project/project.module.ts
+++ ui/project-managment-admin/src/app/project/project.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProjectMainViewComponentComponent} from './project-main-view-component.component';
 import {FormCreatorComponent} from './form-creator/form-creator.component';
-import {ViewFormListComponent} from './view-form-list/view-form-list.component';
+
 import {
   MatButtonModule,
   MatCardModule,
