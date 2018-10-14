ae97819aece4ada066f4836a124d313b0812bc95
lakinduakash <lakinduakash@gmail.com>
2018-10-14 20:16:51 +0530

    move component

--- ui/project-managment-admin/src/app/presentation/view-form-list/view-form-list.component.spec.ts
+++ ui/project-managment-admin/src/app/presentation/view-form-list/view-form-list.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ViewFormListComponent } from './view-form-list.component';
+import {ViewFormListComponent} from './view-form-list.component';
 
 describe('ViewFormListComponent', () => {
   let component: ViewFormListComponent;
