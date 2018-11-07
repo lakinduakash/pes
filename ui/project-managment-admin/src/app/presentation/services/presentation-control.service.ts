e0313dcdebebebcb7e25792beb6493c9fa4d70ac
lakinduakash <lakinduakash@gmail.com>
2018-11-07 08:07:08 +0530

    Add functions services

--- ui/project-managment-admin/src/app/presentation/services/presentation-control.service.ts
+++ ui/project-managment-admin/src/app/presentation/services/presentation-control.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from "@angular/fire/firestore";
 import {getPath} from "../../core/model/firstore-path";
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject, Observable, of, Subject} from "rxjs";
 import {AuthService} from "../../auth/auth.service";
 
 @Injectable({
@@ -36,6 +36,76 @@ export class PresentationControlService {
 
 
     return s as Observable<string[]>
+  }
+
+  getRealTimeStates(state, groupId, pid, presentId) {
+    let s = new Subject<any>();
+    this.auth.user.subscribe(
+      user => {
+        if (user != null || user != undefined) {
+          this.firestore.collection(getPath(user.uid, pid, presentId)).doc(presentId).snapshotChanges().subscribe(
+            next => {
+              s.next(next.payload.data)
+
+
+            }
+          )
+        }
+      }
+    )
+
+    return s as Observable<any>
+  }
+
+  setStates(state, groupId, pid, presentId) {
+    this.auth.user.subscribe(
+      user => {
+        if (user != null || user != undefined) {
+          this.firestore.collection(getPath(user.uid, pid, presentId)).doc(presentId).update({
+            currentState: state,
+            currentGroup: groupId
+          })
+        }
+      }
+    )
+
+  }
+
+  getFinishedList(pid, presentId) {
+    let s = new Subject<any>();
+    this.auth.user.subscribe(
+      user => {
+        if (user != null || user != undefined) {
+          this.firestore.collection(getPath(user.uid, pid, presentId)).doc(presentId).get().subscribe(
+            next => {
+              let list = next.data().finishedList;
+              if (list == undefined)
+                s.next([])
+              else
+                s.next(list)
+
+
+            }
+          )
+        }
+      }
+    )
+
+    return s as Observable<any[]>
+  }
+
+
+  setFinishedList(finishedList, pid, presentId) {
+    this.auth.user.subscribe(
+      user => {
+        if (user != null || user != undefined) {
+          return this.firestore.collection(getPath(user.uid, pid, presentId)).doc(presentId).update({finishedList: finishedList})
+        }
+        else
+          return of(null)
+      }
+    )
+
   }
 
 
