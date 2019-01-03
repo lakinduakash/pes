17d5277faf0ca78358723096185e5eeebae0c1bc
lakinduakash <lakinduakash@gmail.com>
2019-01-04 03:24:52 +0530

    Add component and services

--- ui/project-managment-admin/src/app/marking-view/marking-view.module.ts
+++ ui/project-managment-admin/src/app/marking-view/marking-view.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MarkingViewComponent} from './marking-view/marking-view.component';
+import {MarkingService} from "./services/marking-service.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MarkingViewComponent],
+  providers: [MarkingService],
+  exports: [MarkingViewComponent]
 })
 export class MarkingViewModule {
 }
