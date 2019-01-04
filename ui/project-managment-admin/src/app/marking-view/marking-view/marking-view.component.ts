25321e254bbb9d75f7a5bd2a4f2f5e2a287ff697
lakinduakash <lakinduakash@gmail.com>
2019-01-05 03:31:27 +0530

    Show individual marks

--- ui/project-managment-admin/src/app/marking-view/marking-view/marking-view.component.ts
+++ ui/project-managment-admin/src/app/marking-view/marking-view/marking-view.component.ts
@@ -12,6 +12,12 @@ export class MarkingViewComponent implements OnInit {
 
   oid
 
+  tableObjects = []
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+
   constructor(public markingService: MarkingService, public router: ActivatedRoute, public projectService: ProjectService, public route: Router) {
     this.router.params.subscribe(param => {
       this.oid = param.oid
@@ -27,13 +33,29 @@ export class MarkingViewComponent implements OnInit {
 
   ngOnInit() {
 
-    this.markingService.getAllPresentationIds(this.oid).subscribe(next => console.log(next))
+    this.markingService.getAllPresentationIds(this.oid).subscribe(prIds => {
+      this.markingService.getAllGroupMarksOfPresentation(this.oid, prIds[0]).subscribe(next => {
+        let a = this.convertToTableFormat(next)
+        console.log(a)
+        if (a)
+          this.tableObjects.push(a)
+        this.tableObjects.forEach(item => {
+          console.log(item.colsDef)
+        })
+      })
+
+    })
   }
 
 
   convertToTableFormat(item: GroupMarkPresentation) {
     let groupMembers: string[];
-    let tableObject = []
+    let tableObject = [];
+
+    let evalIds = ['stuId'];
+
+    item.forms.forEach(form => evalIds.push(form.assign.email))
+
 
     if (item.forms && item.forms[0]) {
       groupMembers = Object.keys(item.forms[0].individualMarkMap)
@@ -42,17 +64,54 @@ export class MarkingViewComponent implements OnInit {
     if (groupMembers) {
 
       groupMembers.forEach(mem => {
-        let obj = {}
-        item.forms.forEach(form => {
-          let key = form.assign.email;
-          obj = {...obj, ...{key: form.individualMarkMap[mem]}}
-        });
+        let m = new Map()
+        m.set('stuId', mem);
 
-        tableObject.push(obj)
+        item.forms.forEach(form => {
+          m.set(form.assign.email, form.individualMarkMap[mem])
+
+        });
+        let ob = map_to_obj(m)
+        tableObject.push(ob)
       })
     }
-    console.log(tableObject)
+    if (tableObject.length > 0) {
+      return {dataSource: tableObject, colsDef: evalIds, group: item.group}
+    }
+
+    return null
+
 
   }
 
+
+}
+
+export const map_to_obj = (aMap => {
+  const obj = {};
+  aMap.forEach((v, k) => {
+    obj[k] = v
+  });
+  return obj;
+});
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
 }
