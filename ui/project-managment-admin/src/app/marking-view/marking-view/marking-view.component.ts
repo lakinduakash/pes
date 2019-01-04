bf90fa9380e6a1175dd8abc7d83372415c53ad72
lakinduakash <lakinduakash@gmail.com>
2019-01-04 21:43:26 +0530

    Function to convert data to table format

--- ui/project-managment-admin/src/app/marking-view/marking-view/marking-view.component.ts
+++ ui/project-managment-admin/src/app/marking-view/marking-view/marking-view.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {MarkingService} from "../services/marking-service.service";
+import {GroupMarkPresentation, MarkingService} from "../services/marking-service.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProjectService} from "../../services/project.service";
 
 @Component({
   selector: 'app-marking-view',
@@ -8,11 +10,49 @@ import {MarkingService} from "../services/marking-service.service";
 })
 export class MarkingViewComponent implements OnInit {
 
-  constructor(public markingService: MarkingService) {
+  oid
+
+  constructor(public markingService: MarkingService, public router: ActivatedRoute, public projectService: ProjectService, public route: Router) {
+    this.router.params.subscribe(param => {
+      this.oid = param.oid
+      this.projectService.isProjectExist(undefined, param.oid).subscribe(exist => {
+        if (!exist) {
+          this.route.navigate(['notfoundproject'])
+        }
+      })
+    })
   }
 
+  marks = []
+
   ngOnInit() {
-    this.markingService.getAllGroupMarksOfPresentation('SFmDmTn08wUfRfwBz5e8', 'sg3w5sqjIklRt5RDEfYZ').subscribe(next => console.log(next))
+
+    this.markingService.getAllPresentationIds(this.oid).subscribe(next => console.log(next))
+  }
+
+
+  convertToTableFormat(item: GroupMarkPresentation) {
+    let groupMembers: string[];
+    let tableObject = []
+
+    if (item.forms && item.forms[0]) {
+      groupMembers = Object.keys(item.forms[0].individualMarkMap)
+    }
+
+    if (groupMembers) {
+
+      groupMembers.forEach(mem => {
+        let obj = {}
+        item.forms.forEach(form => {
+          let key = form.assign.email;
+          obj = {...obj, ...{key: form.individualMarkMap[mem]}}
+        });
+
+        tableObject.push(obj)
+      })
+    }
+    console.log(tableObject)
+
   }
 
 }
