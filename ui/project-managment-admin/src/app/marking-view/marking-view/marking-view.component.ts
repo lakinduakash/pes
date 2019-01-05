ac8f5912a27b53183f14aac7a15436c1804b8781
lakinduakash <lakinduakash@gmail.com>
2019-01-05 08:21:01 +0530

    Show marks on groups

--- ui/project-managment-admin/src/app/marking-view/marking-view/marking-view.component.ts
+++ ui/project-managment-admin/src/app/marking-view/marking-view/marking-view.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {GroupMarkPresentation, MarkingService} from "../services/marking-service.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ProjectService} from "../../services/project.service";
+import {PresentationService} from "../../services/presentation.service";
 
 @Component({
   selector: 'app-marking-view',
@@ -14,11 +15,15 @@ export class MarkingViewComponent implements OnInit {
 
   tableObjects = []
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  presentations = []
 
 
-  constructor(public markingService: MarkingService, public router: ActivatedRoute, public projectService: ProjectService, public route: Router) {
+  constructor(public markingService: MarkingService,
+              public router: ActivatedRoute,
+              public projectService: ProjectService,
+              public route: Router,
+              public presentationService: PresentationService) {
+
     this.router.params.subscribe(param => {
       this.oid = param.oid
       this.projectService.isProjectExist(undefined, param.oid).subscribe(exist => {
@@ -34,15 +39,14 @@ export class MarkingViewComponent implements OnInit {
   ngOnInit() {
 
     this.markingService.getAllPresentationIds(this.oid).subscribe(prIds => {
-      this.markingService.getAllGroupMarksOfPresentation(this.oid, prIds[0]).subscribe(next => {
-        let a = this.convertToTableFormat(next)
-        console.log(a)
-        if (a)
-          this.tableObjects.push(a)
-        this.tableObjects.forEach(item => {
-          console.log(item.colsDef)
-        })
-      })
+
+      prIds.forEach(id => {
+          this.presentationService.getPresentationByOriginalProjectId(this.oid, id).subscribe(next => {
+            this.presentations.push({name: next.name, id: id})
+          })
+        }
+      )
+
 
     })
   }
@@ -74,6 +78,16 @@ export class MarkingViewComponent implements OnInit {
         let ob = map_to_obj(m)
         tableObject.push(ob)
       })
+
+      let m = new Map()
+      m.set('stuId', 'Group Mark');
+
+      item.forms.forEach(form => {
+        m.set(form.assign.email, form.currentMark)
+
+      });
+      let ob = map_to_obj(m)
+      tableObject.push(ob)
     }
     if (tableObject.length > 0) {
       return {dataSource: tableObject, colsDef: evalIds, group: item.group}
@@ -81,6 +95,21 @@ export class MarkingViewComponent implements OnInit {
 
     return null
 
+
+  }
+
+  tabChange(index) {
+
+    this.tableObjects = [];
+    this.markingService.getAllGroupMarksOfPresentation(this.oid, this.presentations[index].id).subscribe(next => {
+      let a = this.convertToTableFormat(next)
+      console.log(a)
+      if (a)
+        this.tableObjects.push(a)
+      this.tableObjects.forEach(item => {
+        console.log(item.colsDef)
+      })
+    })
 
   }
 
@@ -95,23 +124,3 @@ export const map_to_obj = (aMap => {
   return obj;
 });
 
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
