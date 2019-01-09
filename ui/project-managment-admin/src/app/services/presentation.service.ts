1c2536c88f965157e708a0069b119479f80047f2
lakinduakash <lakinduakash@gmail.com>
2019-01-09 07:42:56 +0530

    Add documentation

--- ui/project-managment-admin/src/app/services/presentation.service.ts
+++ ui/project-managment-admin/src/app/services/presentation.service.ts
@@ -15,6 +15,7 @@ export class PresentationService {
 
   constructor(public fireStore: AngularFirestore,private authS:AuthService) {
 
+    //Store the uid on service creation
     authS.user.subscribe(
       next => {
         if (next != null)
@@ -26,45 +27,54 @@ export class PresentationService {
     )
   }
 
-  addPresentation(pid: number, data: PresentationData)
-  {
+  /**
+   * Add new presentation to project document
+   * @param pid Simplified project id
+   * @param data presentation data
+   */
+  addPresentation(pid: number, data: PresentationData) {
     let s:Subject<DocumentReference>=new Subject<DocumentReference>();
 
+    //Get the original project id and then save data to presentation collection
     this.fireStore.collection(`usersC/${this.uid}/project`).ref.where('id', '==', pid)
       .onSnapshot(next=>{console.log(next.docs);
         console.log(this.uid);
-        next.docs.forEach(item=>
-        {
-          console.log(item.id);
-          this.fireStore.collection(`usersC/${this.uid}/project/${item.id}/presentation`).add(data).then(next=>
-          s.next(next))
-        }
+        next.docs.forEach(item=> {
+            console.log(item.id);
+            this.fireStore.collection(`usersC/${this.uid}/project/${item.id}/presentation`).add(data).then(next=>
+              s.next(next))
+          }
         )}
       );
 
     return s as Observable<DocumentReference>;
   }
 
-  getPresentation(pid:number,id?:string)
-  {
-    if(id==undefined)
-    {
+  /**
+   * Get presentation data
+   * If presentation doc id is not defined ffunction will return all the presentation list
+   * @param pid Simplified project id
+   * @param id presentation doc id
+   */
+  getPresentation(pid:number, id?:string) {
+    //Check presentation id is defined
+    if(id==undefined) {
       let fpid: Subject<QuerySnapshot<any>> = new Subject();
 
-        this.fireStore.collection(`usersC/${this.uid}/project`).ref.where('id', '==', pid)
-          .onSnapshot(next=>
-            next.docs.forEach(item=>{
+      this.fireStore.collection(`usersC/${this.uid}/project`).ref.where('id', '==', pid)
+        .onSnapshot(next=>
+          next.docs.forEach(item=>{
               this.fireStore.collection(`usersC/${this.uid}/project/${item.id}/presentation`).get().subscribe(next=>
-              fpid.next(next))
+                fpid.next(next))
 
-              }
-            )
-          );
+            }
+          )
+        );
 
       return fpid as Observable<QuerySnapshot<any>>;
     }
-    else
-    {
+    //If presentation id is not defined it will return the all documents in project presentation collection
+    else {
       let fpid: Subject<any> = new Subject();
 
       this.fireStore.collection(`usersC/${this.uid}/project`).ref.where('id', '==', pid)
@@ -73,8 +83,7 @@ export class PresentationService {
               this.fireStore.collection(`usersC/${this.uid}/project/${item.id}/presentation`).doc(id).get().subscribe(
                 next=>{
                   fpid.next(next)
-                },error1 =>
-                {
+                }, error1 => {
                   fpid.error(error1)
                 }
               )
@@ -86,6 +95,11 @@ export class PresentationService {
     }
   }
 
+  /**
+   * Get presentation doc by original project id
+   * @param opid
+   * @param prid
+   */
   getPresentationByOriginalProjectId(opid, prid) {
     let fpid: Subject<any> = new Subject();
 
@@ -102,11 +116,14 @@ export class PresentationService {
   }
 
 
-
+  /**
+   * Delete presentation
+   * @param pid Simplified project number
+   * @param id presentation id
+   */
   deletePresentation(pid:number,id:string)
   {
-    let fpid:Subject<any>=new Subject();
-
+    let fpid: Subject<any> = new Subject()
     this.fireStore.collection(`usersC/${this.uid}/project`).ref.where('id', '==', pid)
       .get().then(next =>
         next.docs.forEach(item=>{
@@ -121,6 +138,10 @@ export class PresentationService {
     return fpid;
   }
 
+  /**
+   * Get list of presentation
+   * @param pid Simplified project id
+   */
   getPresentationList(pid: number) {
     let fpid: Subject<QuerySnapshot<any>> = new Subject();
 
